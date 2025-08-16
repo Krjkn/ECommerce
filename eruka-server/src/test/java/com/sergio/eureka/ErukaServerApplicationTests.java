b7b03ef3b5edff4ca305f711cf705cc94533737e
Sergio Angel Palacios Castro <sergiokrpc13@gmail.com>
2025-08-16 09:23:31 -0600

    Adición del Servidor de Eruka

---
+++ eruka-server/src/test/java/com/sergio/eureka/ErukaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sergio.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErukaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
