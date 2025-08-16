de778281cdc21c5cc67ae7a0955dc2fdf12fbbd2
Sergio Angel Palacios Castro <sergiokrpc13@gmail.com>
2025-08-16 09:54:00 -0600

    Configuración inicial de los microservicios de Productos y Pedidos

---
+++ msv-productos/src/main/java/com/sad/productos/MsvProductosApplication.java
@@ -0,0 +1,13 @@
+package com.sad.productos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvProductosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvProductosApplication.class, args);
+	}
+
+}
---
+++ msv-productos/src/test/java/com/sad/productos/MsvProductosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sad.productos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvProductosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
